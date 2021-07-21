f4f2045ef3b67d90a386dd9e0b83f8e841752ae7
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-21 13:10:34 +0530

    Installed @material-tailwind and imported Button and I con components

---
+++ my-next-app/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
+const Header = () => {
+    return (
+        <div className="flex items-center">
+            <Button
+                color='gray'
+                buttonType="outline"
+                rounded={true}
+                iconOnly={true}
+                ripple="dark"
+                className="hidden md:inline-flex h-20 w-20 border-0"
+            >
+                <Icon name="menu" size="3xl" />
+            </Button>
+        </div>
+    )
+}
+
+export default Header
