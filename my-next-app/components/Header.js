f38159d22d537c9e630ed24920f8c7852751be8f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-23 11:11:17 +0530

    Added icon and header & made the div sticky

--- my-next-app/components/Header.js
+++ my-next-app/components/Header.js
@@ -4,17 +4,19 @@ import Icon from "@material-tailwind/react/Icon";
 
 const Header = () => {
     return (
-        <div className="flex items-center">
+        <div className="sticky top-0 z-50 flex items-center px-4 py-2 shadow-md bg-white">
             <Button
                 color='blue'
                 buttonType="outline"
                 rounded={true}
                 iconOnly={true}
                 ripple="dark"
-                className="md:inline-flex h-20 w-20 border-0"
+                className="md:inline-flex h-10 w-10 border-0"
             >
-                <Icon name="menu" size="3xl" />
+                <Icon name="menu" size="2xl" />
             </Button>
+            <Icon name="description" size="5xl" color="blue" />
+            <h1 className="md:inline-flex ml-2 text-gray-700 text-2xl">Docs</h1>
         </div>
     )
 }
