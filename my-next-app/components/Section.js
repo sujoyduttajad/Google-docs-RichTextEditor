1750a99cd608e17886828a415fb28d86cdcfb707
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-28 16:47:29 +0530

    Update Section.js

--- my-next-app/components/Section.js
+++ my-next-app/components/Section.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Section = () => {
     return (
-        <div>
-            
-        </div>
+        <section className="bg-[#F8F9FA] pb-10 px-10">
+            <div className="max-w-3xl mx-auto">
+                <div>
+                    <h2>Start a new document</h2>
+                </div>
+            </div>
+        </section>
     )
 }
 
