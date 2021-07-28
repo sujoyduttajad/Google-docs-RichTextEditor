75741f4acbc1e781c8d4221cf1f06c509aaede73
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-28 16:11:19 +0530

    Update Header.js

--- my-next-app/components/Header.js
+++ my-next-app/components/Header.js
@@ -11,17 +11,29 @@ const Header = () => {
                 rounded={true}
                 iconOnly={true}
                 ripple="dark"
-                className="md:inline-flex h-10 w-10 border-0 px-2"
+                className="md:inline-flex h-20 w-20 border-0 px-2"
             >
                 <Icon name="menu" size="3xl" />
             </Button>
             <Icon name="description" size="5xl" color="blue" />
             <h1 className="md:inline-flex ml-2 text-gray-700 text-2xl mx-2">Docs</h1>
 
-            <div className="flex flex-grow items-center p-5 py-2 bg-gray-100 mx-5 text-gray-600 rounded-lg">
+            <div className="flex flex-grow items-center p-5 py-2 bg-gray-100 
+            mx-5 md:mx-20 text-gray-600 rounded-lg focus-within:text-gray-600 focus-within:shadow-md">
                 <Icon name="search" size="3xl" color="darkgray" />
                 <input type="text" placeholder="Search" className="flex-grow px-5 text-base bg-transparent outline-none" />
             </div>
+
+            <Button
+                color='gray'
+                buttonType="outline"
+                rounded={true}
+                iconOnly={true}
+                ripple="dark"
+                className=" md:inline-flex ml-5 md:ml-20 h-20 w-20 border-0"
+            >
+                <Icon name="apps" size="3xl" />
+            </Button>
         </div>
     )
 }
