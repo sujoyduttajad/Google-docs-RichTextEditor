1949c855927304573b4f2b63a1be85d182dda2da
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-28 16:35:44 +0530

    Added profile picture avatar

--- my-next-app/components/Header.js
+++ my-next-app/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                 rounded={true}
                 iconOnly={true}
                 ripple="dark"
-                className="md:inline-flex h-20 w-20 border-0 px-2"
+                className="md:inline-flex h-20 w-20 border-0"
             >
                 <Icon name="menu" size="3xl" />
             </Button>
@@ -34,6 +34,13 @@ const Header = () => {
             >
                 <Icon name="apps" size="3xl" />
             </Button>
+
+            <img
+                loading="lazy"
+                className="cursor-pointer h-12 w-12 rounded-full ml-2"
+                src="https://lh3.googleusercontent.com/-LFXiL6jSAXU/WvY7H3c10pI/AAAAAAAAAjs/ChFr0JdTTD8KPwdp0SutGd4bGxcxw-_gwCEwYBhgLKtMDABHVOhxzzy0d76X8tPbaolaRW2OWxP-5vIiVoq8rlyo9iQ4yqu3SQippK0GSQ4KhPquJm_gIW0RmdBeVTmT-JXVY1Lxv8tZvYW_aanh-oNf-H5EiH52uYmJYxoqHdjrX_GdvB1iUWhcIeISbzGz93UwDBwLbVdlOxU5scCPFl3DRXsjORsJUGe0M0AipvtXO9rCRgYuBEqXwuuM8p-nYZvRhUxug5wbdDw6eVQMSO0StBRrYUcst8yucdE7nUd-KWew5Qs5Qn_f_8iwsoC3LSWlvHTHBgYXeeFBeEhEaOPBs1eZUQrxl104WdPXMt8mcCkDh-lgyS2nYBnNCynMQmzHQh6CHwdfi8yj0R15O3ecz_5K8Ylqeccsfknc-BjBYh4fRw4EwJkGLLox0PqZDy7HHkK5JxO9zZOHdro2xDyD6HjoBuky3i4FB7zmLdzAdIZ6iTpI7-AJ78uWKwPQWFjfpIdkEb_4M4-IEJT8f1yEuNWysYxD-Pu201Zvn6OXfqZVMh2uILAkugz4nJOQK-WpC65Jq_oy0Q96DNbEd8YAe8USSraw6OCTCVp1NXuuwVeG8jBJbdEpcG7mJNjessDMxZ7bPKdksn4NpR6SEOQcgx3Aw_feEiAY/w140-h140-p/1cc3b04d-4e86-463e-8630-ce661c29889c"
+                alt="profile picture"
+            />
         </div>
     )
 }
