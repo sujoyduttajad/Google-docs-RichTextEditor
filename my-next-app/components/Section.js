54e8a0276aeb3ef43cf6c8195beb4a76895aa8d1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-30 13:02:35 +0530

    Update Section.js

--- my-next-app/components/Section.js
+++ my-next-app/components/Section.js
@@ -4,8 +4,8 @@ const Section = () => {
     return (
         <section className="bg-[#F8F9FA] pb-10 px-10">
             <div className="max-w-3xl mx-auto">
-                <div>
-                    <h2>Start a new document</h2>
+                <div className="flex items-center justify-between py-6">
+                    <h2 className="text-gray-700 text-lg">Start a new document</h2>
                 </div>
             </div>
         </section>
