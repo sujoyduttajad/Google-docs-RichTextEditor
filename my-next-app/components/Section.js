6eeea5eba5daeb567c4c4436d3b819638c27efd2
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-31 15:26:48 +0530

    Update Section.js

--- my-next-app/components/Section.js
+++ my-next-app/components/Section.js
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 const Section = () => {
     return (
-        <section className="bg-[#F8F9FA] pb-10 px-10">
+        <section className="bg-[#F8F9FA] pb-10 px-10 h-20vh">
             <div className="max-w-3xl mx-auto">
                 <div className="flex items-center justify-between py-6">
                     <h2 className="text-gray-700 text-lg">Start a new document</h2>
@@ -22,10 +22,10 @@ const Section = () => {
                     </Button>
                 </div>
                 <div>
-                    <div className="relative h-52 w-40">
+                    <div className="relative h-52 w-40 border-2 cursor-pointer hover:border-blue-700">
                         <Image src="https://links.papareact.com/pju" layout="fill" />
                     </div>
-                    
+
                     <p>Blank</p>
                 </div>
             </div>
