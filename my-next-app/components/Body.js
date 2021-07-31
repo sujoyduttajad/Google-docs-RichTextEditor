640d9b77399cd32a22e9e03836ea276e724a560b
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-31 17:06:01 +0530

    Create Body.js

---
+++ my-next-app/components/Body.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <section>
+            <div className="max-w-3xl mx-auto py-8">
+                <div>
+                    <h2>My Documents</h2>
+                    <p>Date Created</p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Body
