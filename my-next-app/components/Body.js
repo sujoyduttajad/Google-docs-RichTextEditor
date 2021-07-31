b551d636bb395aecc07579bc4eb0eb5b804bef1f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-07-31 17:16:19 +0530

    Update Body.js

--- my-next-app/components/Body.js
+++ my-next-app/components/Body.js
@@ -4,11 +4,11 @@ import Button from "@material-tailwind/react/Button";
 
 const Body = () => {
     return (
-        <section>
-            <div className="max-w-3xl mx-auto py-8">
-                <div>
-                    <h2>My Documents</h2>
-                    <p>Date Created</p>
+        <section className="bg-white px-10 md:px-0">
+            <div className="max-w-3xl mx-auto py-8 text-sm text-gray-700">
+                <div className="flex item-center justify-between pb-5">
+                    <h2 className="font-medium flex-grow">My Documents</h2>
+                    <p className="mr-12">Date Created</p>
                     <Icon name="folder" size="3xl" color="gray" />
                 </div>
             </div>
