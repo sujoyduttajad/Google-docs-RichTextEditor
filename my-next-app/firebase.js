30db0e4295020635a79e17cedfdccd538f0a88ba
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-22 11:23:25 +0530

    Created Firebase.js config file

---
+++ my-next-app/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5NtpxkR3tqV85-nijyw_TErJwD9dE4iM",
+    authDomain: "docs-d650d.firebaseapp.com",
+    projectId: "docs-d650d",
+    storageBucket: "docs-d650d.appspot.com",
+    messagingSenderId: "919108693715",
+    appId: "1:919108693715:web:e6727d81bba62d4a7c6e26"
+  };
