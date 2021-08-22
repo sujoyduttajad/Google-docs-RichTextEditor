4d3cf0ae3d43c3d49559c45dcc10e51402a21b45
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-08-22 12:31:14 +0530

    Update firebase.js

--- my-next-app/firebase.js
+++ my-next-app/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB5NtpxkR3tqV85-nijyw_TErJwD9dE4iM",
     authDomain: "docs-d650d.firebaseapp.com",
@@ -6,3 +8,16 @@ const firebaseConfig = {
     messagingSenderId: "919108693715",
     appId: "1:919108693715:web:e6727d81bba62d4a7c6e26"
   };
+
+// Here connect Firebase to the app
+// Typically we use firebase.InitializeApp() function when we are using react
+// but with Next.js when it is SSR sometimes it might already initialized the app 
+// so in that case it ends up double initializeing the app 
+// To avoid double initialization we are using the below code logic
+// So if there is no app --> then pass in the firebaseConfig --> otherwise we gonna use what we already have
+  const app = !firebase.apps.length 
+    ? firebase.initializeApp(firebaseConfig)
+    : firebase.app();
+
+
+// Now we will create    
