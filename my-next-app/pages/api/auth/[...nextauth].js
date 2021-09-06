caf3730abe9ef81d0116e816146d3a53e77f9b76
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-06 15:42:32 +0530

    Added FirebaseAdapter to NextAuth

--- my-next-app/pages/api/auth/[...nextauth].js
+++ my-next-app/pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 
 import NextAuth from "next-auth"
 import GoogleProvider from `next-auth/providers/google`
+import { FirebaseAdapter } from '@next-auth/firebase-adapter'
+import { db } from '../../../firebase'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +13,5 @@ export default NextAuth({
       }),
     // ...add more providers here
   ],
+  adapter: FirebaseAdapter(db)
 })
