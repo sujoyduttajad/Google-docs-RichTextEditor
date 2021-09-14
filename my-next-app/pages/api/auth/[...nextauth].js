648ab06908b741d17a091d9dada7dc8e04b0233e
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-09-14 13:35:52 +0530

    Installed next-auth

--- my-next-app/pages/api/auth/[...nextauth].js
+++ my-next-app/pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 
 import NextAuth from "next-auth"
-import GoogleProvider from `next-auth/providers/google`
+import GoogleProvider from "next-auth/providers/google";
 import { FirebaseAdapter } from '@next-auth/firebase-adapter'
 import { db } from '../../../firebase'
 
