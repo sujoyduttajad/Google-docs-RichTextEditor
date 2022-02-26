c53c88595b6612dbc2fbe0ab09a2993ff5cad39f
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-02-26 17:56:10 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import 'tailwindcss/tailwind.css'
 import "@material-tailwind/react/tailwind.css";
+import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
