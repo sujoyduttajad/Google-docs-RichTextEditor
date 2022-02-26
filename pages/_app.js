245de8b10d41af7c44c3b1555b7994c00e7b74f9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-02-26 18:37:58 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,20 @@
 import "@material-tailwind/react/tailwind.css";
 import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
+import { Provider } from "next-auth/client";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <link
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet"
-      />
-    </Head>
-      <Component {...pageProps} />
+      <Head>
+        <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+        />
+      </Head>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
     </>
   );
 }
